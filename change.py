a78a712c49666b291ac74bacf8da5e6e0191b3f7
emilyviader <emily.viader@gmail.com>
2024-08-13 17:15:13 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,14 @@
 def change():
     expense = 23.75
     money = 100
+    
+    returned = money - expense
+    
+    dollars = int(returned)
+    cents = int((returned - dollars) * 100)
+
+    print("\nReturned\n")
+    print(f"Dollars:\n{dollars}")
+    print(f"Cents:\n{cents}")
+
+change()
