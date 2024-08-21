78719cd5e7ba729df4e3ecd477cfb9bd07bcc33d
emilyviader <emily.viader@gmail.com>
2024-08-20 22:55:44 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -2,12 +2,10 @@ def earth():
     x = "Bangladesh"
     y = "Barbados"
 
-    format1 = f"The result of {X} comes first in the dictionary than {Y} is {(X < Y)}"
-    
-    format2 = f"The result of {Y} comes first in the dictionary than {X} is {(X > Y)}"
-    
-    print(str(format1)) 
-    
-    print(str(format2))
+    format1 = f"The result of {x} comes first in the dictionary than {y} is {x < y}"
+    format2 = f"The result of {y} comes first in the dictionary than {x} is {y < x}"
+
+    print(format1) 
+    print(format2)
 
 earth()
