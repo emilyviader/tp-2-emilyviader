7ae712f57516e02decb05d00df22c0d42924c11f
emilyviader <emily.viader@gmail.com>
2024-08-24 18:00:12 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,17 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+
+    format1 = f"{first_name.lower()} {last_name.lower()}"
+    format2 = f"{first_name.capitalize()} {last_name.capitalize()}"
+    format3 = f"{first_name.upper()} {last_name.upper()}"
+    format4 = f"\t{first_name.lower()} {last_name.lower()}"
+
+    print(format1)
+    print(format2)
+    print(format3)
+    print(format4)
+
+# Call the function to execute it
+ada()
+
